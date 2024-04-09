f3b023a0543e88a8b857e932f406e0fef911cd1b
LadyPhoenixEternity <164013321+LadyPhoenixEternity@users.noreply.github.com>
2024-04-09 11:04:46 +0300

    Update lib.h

--- lib.h
+++ lib.h
@@ -8,13 +8,10 @@ void welcome(string name)
   cout << "Welcome to the Brain Games!" << "\n";
   cout << "May I have your name?" << "\n";
   cout << "Hello, " + name + "!" << "\n";
-  cout << "Find the smallest common multiple of given nimbers." << "\n";
-  
-  cout << "Hello " << name << "\n";
   
 }
 
 int generate() {
   
-  return rand() % 30 + 1;
+  return rand() % 10 + 1;
 }
