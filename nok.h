b2c32a1ef0838389cbe6ff1fc5d83a1ce3f05b8d
LadyPhoenixEternity <164013321+LadyPhoenixEternity@users.noreply.github.com>
2024-04-09 10:51:38 +0300

    Update nok.h

--- nok.h
+++ nok.h
@@ -1,6 +1,12 @@
 int nok(int a, int b, int c)
 {
-    int maxs = std::max({a, b, c});
+    int maxs;
+    if (a > b)
+      maxs = a;
+    else
+      maxs = b;
+    if (c > maxs)
+      maxs = c;
     while (true) {
         if (maxs % a == 0 && maxs % b == 0 && maxs % c == 0) {
             break;
